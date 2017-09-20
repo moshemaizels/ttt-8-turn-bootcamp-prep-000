0fdc1f2f2a7658022131338c06c2d1851d965d9c
Moshe Maizels <moshemaizels@gmail.com>
2017-09-20 13:32:52 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,25 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if index < 0 || index > 8
+    false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
+  end
+end
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+  #  puts"falseY"
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+
+end
+
